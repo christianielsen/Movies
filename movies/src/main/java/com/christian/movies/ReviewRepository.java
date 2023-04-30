128977938e34e47fd8b93f4bcbc04bcc26a8a8a6
Chris <chubby1226@gmail.com>
2023-04-30 20:09:33 +0100

    Initial commit

---
+++ movies/src/main/java/com/christian/movies/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.christian.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
